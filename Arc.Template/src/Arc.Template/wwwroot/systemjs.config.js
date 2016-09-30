c145a7d6ad1735265b7f264d9be5619483a9bc89
j.stewart <J.Stewart@thelondonclinic.co.uk>
2016-09-30 08:10:37 +0100

    Initial Commit

---
+++ Arc.Template/src/Arc.Template/wwwroot/systemjs.config.js
@@ -0,0 +1,50 @@
+﻿(function (global) {
+
+    var paths = {
+        // paths serve as alias
+        'lib:': 'libs/'
+    };
+
+    var map = {
+        'app': 'app',
+        'rxjs': 'lib:rxjs',
+        '@angular': 'lib:@angular',
+        'angular-in-memory-web-api': 'lib:angular-in-memory-web-api'
+    };
+
+    var packages = {
+        'app': { main: 'main.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' },
+        'angular-in-memory-web-api': { defaultExtension: 'js' },
+    };
+
+    var packageNames = [
+      '@angular/common',
+      '@angular/compiler',
+      '@angular/core',
+      '@angular/forms',
+      '@angular/http',
+      '@angular/platform-browser',
+      '@angular/platform-browser-dynamic',
+      '@angular/router',
+      '@angular/upgrade'
+    ];
+
+    packageNames.forEach(function (pkgName) {
+        var p = pkgName.replace('@angular/', '');
+        packages[pkgName] = { main: 'bundles/' + p + '.umd.js', defaultExtension: 'js' };
+        //packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    var config = {
+        paths: paths,
+        map: map,
+        packages: packages
+    };
+
+    // filterSystemConfig - index.html's chance to modify config before we register it.
+    //if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+
+    System.config(config);
+
+})(this);
